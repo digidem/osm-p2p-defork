89c2452712dfd7af992ba8d5adcb0ebd2da5b296
Stephen Whitmore <sww@eight45.net>
2017-03-23 15:36:30 -0700

    remove heavy dev deps; simpler unit tests

--- test/fork.js
+++ test/fork.js
@@ -1,120 +1,52 @@
 var test = require('tape')
-var hyperlog = require('hyperlog')
-var fdstore = require('fd-chunk-store')
-var path = require('path')
-var memdb = require('memdb')
-
-var tmpdir = require('os').tmpdir()
-var storefile0 = path.join(tmpdir, 'osm-store-' + Math.random())
-var storefile1 = path.join(tmpdir, 'osm-store-' + Math.random())
-
-var osmdb = require('osm-p2p-db')
-
 var defork = require('../')
 
 // Contains two versions of a node inside a way; the one with the newer
 // timestamp should be included.
 test('fork', function (t) {
-  t.plan(10)
-  var osm0 = osmdb({
-    log: hyperlog(memdb(), { valueEncoding: 'json' }),
-    db: memdb(),
-    store: fdstore(4096, storefile0)
-  })
-  var osm1 = osmdb({
-    log: hyperlog(memdb(), { valueEncoding: 'json' }),
-    db: memdb(),
-    store: fdstore(4096, storefile1),
-    size: 4096
-  })
-  var docs = {
-    A: { type: 'node', lat: 64.5, lon: -147.3 },
-    B: { type: 'node', lat: 63.9, lon: -147.6 },
-    C: { type: 'node', lat: 64.2, lon: -146.5 },
-    D: { type: 'way', refs: [ 'A', 'B', 'C' ] }
-  }
-  var names = {}
-  var nodes = {}
-  var versions = {}
+  var input = [
+    { type: 'node',
+      lat: 63.9,
+      lon: -147.6,
+      timestamp: 100,
+      id: '2552852387437165718',
+      version: 'b46af8c3794cc2be4177821f6ecd0dc9f6b3684f24375a94435eeb19d21c1a34' },
+    { type: 'node',
+      lat: 62.4,
+      lon: -146.3,
+      timestamp: 105,
+      id: '3688784127941314493',
+      version: 'bc0012fd7acb05597cf0f5792108fd936062ecafce440a51c9304ae12e03ef24' },
+    { type: 'node',
+      lat: 62.5,
+      lon: -146.2,
+      timestamp: 101,
+      id: '3688784127941314493',
+      version: '07ef85e47c1e8db10b11342b8bead27ef2b34c8f65f614b5277ae2a2fb768264' },
+    { type: 'node',
+      lat: 64.5,
+      lon: -147.3,
+      timestamp: 100,
+      id: '14820318362135612004',
+      version: 'b3de4a7d6c72f7fa0f8a6e41082e1f67f15ab0cee38c3f8a8c96ac97922d2be5' },
+    { type: 'way',
+      refs:
+      [ '14820318362135612004',
+        '2552852387437165718',
+        '3688784127941314493' ],
+      timestamp: 100,
+      id: '13399941906737957041',
+      version: '1c70645d13105610e993dec8d7e03b7b263959a5784aa1c500b8e817338bbcb9' }
+  ]
 
-  var keys = Object.keys(docs).sort()
-  ;(function next () {
-    if (keys.length === 0) return ready()
-    var key = keys.shift()
-    var doc = docs[key]
-    if (doc.refs) {
-      doc.refs = doc.refs.map(function (ref) { return names[ref] })
-    }
-    doc.timestamp = 100
-    osm0.create(doc, function (err, k, node) {
-      t.ifError(err)
-      names[key] = k
-      if (!versions[key]) versions[key] = []
-      versions[key].push(node.key)
-      nodes[k] = node
-      next()
-    })
-  })()
+  var actual = defork(input)
+  actual.sort(idcmp)
+  var expected = input.slice()
+  expected.splice(2, 1)
+  expected.sort(idcmp)
 
-  function ready () {
-    var r0 = osm0.log.replicate()
-    var r1 = osm1.log.replicate()
-    r0.pipe(r1).pipe(r0)
-    r0.once('end', function () {
-      var newdoc0 = { type: 'node', lat: 62.5, lon: -146.2 }
-      var newdoc1 = { type: 'node', lat: 62.4, lon: -146.3 }
-      newdoc0.timestamp = 101
-      newdoc1.timestamp = 105
-      osm0.put(names.C, newdoc0, function (err, node) {
-        t.ifError(err)
-        versions.C.push(node.key)
-
-        osm1.put(names.C, newdoc1, function (err, node) {
-          t.ifError(err)
-          versions.C.push(node.key)
-          replicate()
-        })
-      })
-    })
-  }
-
-  function replicate () {
-    var r0 = osm0.log.replicate()
-    var r1 = osm1.log.replicate()
-    r0.pipe(r1).pipe(r0)
-    r0.once('end', function () {
-      check()
-    })
-  }
-
-  function check () {
-    var q0 = [[-90,90],[-180,180]]
-    var ex0 = [
-      { type: 'node', lat: 64.5, lon: -147.3,
-        id: names.A, version: versions.A[0] },
-      { type: 'node', lat: 63.9, lon: -147.6,
-        id: names.B, version: versions.B[0] },
-      { type: 'node', lat: 62.5, lon: -146.2,
-        id: names.C, version: versions.C[1] },
-      { type: 'node', lat: 62.4, lon: -146.3,
-        id: names.C, version: versions.C[2] },
-      { type: 'way', refs: [ names.A, names.B, names.C ],
-        id: names.D, version: versions.D[0] }
-    ].sort(idcmp)
-    // osm0.query(q0, function (err, res) {
-    //   t.ifError(err)
-    //   t.deepEqual(res.sort(idcmp), ex0, 'query 0 osm0')
-    // })
-    osm1.query(q0, function (err, res) {
-      t.ifError(err)
-      // t.deepEqual(res.sort(idcmp), ex0, 'query 0 osm1')
-
-      console.log('---')
-      console.log(res)
-      console.log('---')
-      console.log(defork(res))
-    })
-  }
+  t.deepEqual(actual, expected)
+  t.end()
 })
 
 function idcmp (a, b) {
